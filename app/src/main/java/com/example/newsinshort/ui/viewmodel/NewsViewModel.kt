633138b69a9ce53955fb54bd6ea741fbfe0c05b0
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-03-29 13:47:06 +0530

    Fetching data from api and emit whith flow

--- app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsinshort/ui/viewmodel/NewsViewModel.kt
@@ -2,22 +2,46 @@ package com.example.newsinshort.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsinshort.data.AppConstants
+import com.example.newsinshort.data.entity.NewsResponse
+import com.example.newsinshort.ui.repository.NewsRepository
+import com.example.utilities.ResourceState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class NewsViewModel @Inject constructor(): ViewModel(){
-    val value = "value"
+class NewsViewModel @Inject constructor(
+    private val newsRepository: NewsRepository
+) : ViewModel() {
+
+
+    // MutableStateFLow can be assigned from the viewmodel itself
+    private val _news: MutableStateFlow<ResourceState<NewsResponse>> =
+        MutableStateFlow(ResourceState.Loading())
+
+    //Stateflow is exposed to the UI , so UI cannot change the value
+    val news: StateFlow<ResourceState<NewsResponse>> = _news
+
+    fun getNews(country: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            newsRepository.getNewsHeadline(country)
+                .collectLatest { newsResponse ->
+                    _news.value = newsResponse
+                }
+        }
+    }
 
     init {
-        Log.d(TAG,"Init Block of NewsViewModel")
+        getNews(AppConstants.COUNTRY)
     }
 
-    fun doSomething(){
-        println("Do Something")
-    }
-
-    companion object{
+    companion object {
         const val TAG = "NewsViewModel"
     }
 }
