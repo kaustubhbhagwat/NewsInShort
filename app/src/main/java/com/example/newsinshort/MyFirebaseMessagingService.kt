e3d23b3d6b2274b3106c51894cdd05484ab6545e
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-08 20:56:38 +0530

    added FCM support

--- app/src/main/java/com/example/newsinshort/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/newsinshort/MyFirebaseMessagingService.kt
@@ -103,7 +103,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val channelId = "fcm_default_channel"
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.logo)
             .setContentTitle("FCM Message")
             .setContentText(messageBody)
             .setAutoCancel(true)
