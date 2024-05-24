7933a398f880709316394847553fa60835da3f8b
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-24 19:48:05 +0530

    implementing room db

--- app/src/main/java/com/example/newsinshort/data/database/NewsDatabase.kt
+++ app/src/main/java/com/example/newsinshort/data/database/NewsDatabase.kt
@@ -4,7 +4,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.newsinshort.data.database.entities.SavedNews
-import org.junit.runner.manipulation.Ordering.Context
 
 @Database(entities = [SavedNews::class], version = 1)
 abstract class NewsDatabase : RoomDatabase() {
--- app/src/main/java/com/example/newsinshort/data/database/SavedNewsDao.kt
+++ app/src/main/java/com/example/newsinshort/data/database/SavedNewsDao.kt
@@ -2,11 +2,18 @@ package com.example.newsinshort.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.newsinshort.data.database.entities.SavedNews
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface SavedNewsDao{
+interface SavedNewsDao {
 
     @Insert
     suspend fun insertSavedNews(savedNews: SavedNews)
+
+    @Query("SELECT * FROM SAVED_NEWS")
+    fun getAllArticles(): Flow<SavedNews>
+
+
 }
