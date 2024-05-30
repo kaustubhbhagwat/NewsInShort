a4b35c308f1498b07ce19e1765c11959b86fd545
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-30 18:40:26 +0530

    Implementing different screens with BottomBar

---
+++ app/src/main/java/com/example/newsinshort/ui/screens/saved_screen/SavedNewsScreen.kt
@@ -0,0 +1,18 @@
+package com.example.newsinshort.ui.screens.saved_screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
+@Composable
+fun SavedNewsScreen(
+    navController: NavController
+) {
+
+    Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+
+    }
+}
