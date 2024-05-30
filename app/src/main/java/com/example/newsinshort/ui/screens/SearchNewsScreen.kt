d0461680cdc9d582bfb4ef2729c42cd58376a4d4
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-30 18:40:31 +0530

    Implementing different screens with BottomBar

---
+++ app/src/main/java/com/example/newsinshort/ui/screens/SearchNewsScreen.kt
@@ -0,0 +1,16 @@
+package com.example.newsinshort.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@Composable
+fun SearchNewsScreen(navController: NavHostController) {
+
+    Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+
+    }
+}
