2a30a8045d9d621b2118358c14bc17e2eb7674b6
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-30 18:40:16 +0530

    Implementing different screens with BottomBar

---
+++ app/src/main/java/com/example/newsinshort/ui/screens/home_screen/HomeNavGraph.kt
@@ -0,0 +1,49 @@
+package com.example.newsinshort.ui.screens.home_screen
+
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.newsinshort.ui.Graph
+import com.example.newsinshort.ui.screens.SearchNewsScreen
+import com.example.newsinshort.ui.screens.breaking_news.BreakingNewsScreen
+import com.example.newsinshort.ui.screens.news_screen.NewsScreenViewModel
+import com.example.newsinshort.ui.screens.saved_screen.SavedNewsScreen
+import com.example.newsinshort.utils.BottomBarScreen
+
+@Composable
+fun HomeNavGraph(navController: NavHostController) {
+
+    val snackBarState = remember { (SnackbarHostState()) }
+    val argKey = "webUrl"
+
+
+    NavHost(
+        navController = navController,
+        route = Graph.HOME,
+        startDestination = BottomBarScreen.BreakingNews.route
+    ) {
+
+        composable(route = BottomBarScreen.BreakingNews.route) {
+            val viewModel: NewsScreenViewModel = hiltViewModel()
+           BreakingNewsScreen(navController = navController)
+        }
+
+
+        composable(route = BottomBarScreen.SavedNews.route) {
+            SavedNewsScreen(
+                navController = navController,
+            )
+        }
+
+        composable(route = BottomBarScreen.SearchNews.route){
+            SearchNewsScreen(navController = navController)
+        }
+
+    }
+//    }
+
+}
